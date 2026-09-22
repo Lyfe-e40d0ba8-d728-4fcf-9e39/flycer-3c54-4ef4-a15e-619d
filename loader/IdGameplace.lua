26d52e3f9bc8e8913d3a93113c60ee359838ee23
FLYCER-6c06-4dca-954b <ann.vandalize@gmail.com>
2026-09-22 17:39:28 +0700

    Update and rename Loader to loader/IdGameplace.lua

---
+++ loader/IdGameplace.lua
@@ -0,0 +1,10 @@
+return {
+    -- Bakso Malang Anomalies
+    [77338972879392] = "https://pandadevelopment.net/virtual/file/8c4ffff386145ea3",
+    -- Quiet or Die
+    [126922689754590] = "https://pandadevelopment.net/virtual/file/d6f08568f82b9cf7",
+    -- Escape Tsunami For Brainrots
+    [131623223084840] = "https://pandadevelopment.net/virtual/file/ec4519530fcfcb38",
+    -- Scary Shawarma Kiosk: ANOMALY [horror]
+    [128001665358186] = "https://pandadevelopment.net/virtual/file/7d1781dc75daff47",
+}
